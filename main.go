68346a72182a6ca0a2352559d8b3c081cb549899
Andrew Kharook <andriy.kharuk@gmail.com>
2018-12-14 15:31:30 +0200

    [WIP] Basic implementation
    TODO:
    * Add file size to files output
    * Mind the -f flag to show/hide files

--- main.go
+++ main.go
@@ -8,8 +8,47 @@ import (
 	"sort"
 )
 
-func dirTree(out io.Writer, path string, printFiles bool) error {
+func formatOutput(file os.FileInfo, prefix string, isLast bool) string {
+	glyph := "├"
+	if isLast {
+		glyph = "└"
+	}
 
+	return prefix + glyph + "───" + file.Name() + "\n"
+}
+
+func scanDir(path string, getFiles bool, prefix string) (res string) {
+	dir, err := os.Open(path)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if items, err := dir.Readdir(0); err == nil {
+		sort.Slice(items, func(i, j int) bool { return items[i].Name() < items[j].Name() })
+
+		for i := 0; i < len(items); i++ {
+			currFile := items[i]
+			if i == len(items)-1 {
+				res += formatOutput(currFile, prefix, true)
+				if items[i].IsDir() {
+					res += scanDir(filepath.Join(path, currFile.Name()), getFiles, prefix+"\t")
+				}
+			} else {
+				res += formatOutput(currFile, prefix, false)
+				if items[i].IsDir() {
+					res += scanDir(filepath.Join(path, currFile.Name()), getFiles, prefix+"│\t")
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	fmt.Println(scanDir(path, printFiles, ""))
+
+	return nil
 }
 
 func main() {
